3511cbd11580c9e9cffa110cc0ffb7232a846309
YusufHamawandi <yusuf.hamawandi@outlook.com>
2022-04-09 12:10:14 +0200

    new file:   Builds/MeowieClickerV1.exe 	new file:   Logo/Iconka-Meow-2-Cat-sleep.ico 	new file:   Logo/cat-sleep-icon.png 	modified:   README.md 	new file:   Source/MeowieClicker.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MeowieClicker
+A Open Source Minecraft Focused AutoClicker and Utility client.
+Join the Discord Server: https://discord.gg/D9nbH3MSq4
+
---
+++ Source/MeowieClicker.py
@@ -0,0 +1,35 @@
+from pyautogui import click as clickmouse
+from keyboard import is_pressed
+from random import randint
+
+version = "V1-BETA1"
+
+def startup():
+    print("--------------------------------------")
+    print("| MEOWIE CLICKER V1-BETA by FATYCATY |")
+    print("--------------------------------------")
+    print("MEOWIE CLICKER"+ version +" by FATYCATY")
+
+    cps = int(input("CPS[1-8]: "))
+    return cps 
+
+def clicker(cps):
+    click = False
+    while True:
+        if(is_pressed("r")):
+            click = True
+            print(click)
+
+        if(is_pressed("z")):
+            click = False
+            print(click)
+        if(click):
+            clickmouse(interval=1.00/(cps*3), button="left")
+            
+
+def main():
+    clicker(startup())
+
+
+
+main()
